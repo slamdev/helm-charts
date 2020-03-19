3a395f3bd1a6c2cf14e80d0b306fb4d18e6044e4
Valentin Fedoskin <valentin.fedoskin@gmail.com>
2020-03-19 16:02:52 +0100

    fix env regex

--- charts/nginx/files/entrypoint.sh
+++ charts/nginx/files/entrypoint.sh
@@ -4,13 +4,13 @@ set -o errexit
 set -o nounset
 
 NGINX_BIN=nginx
-if [ "${NGINX_DEBUG}" = "foo" ]; then
+if [ "${NGINX_DEBUG}" = "true" ]; then
   NGINX_BIN=nginx-debug
 fi
 
 CONF_DIR=/etc/nginx/
 
 cp "${CONF_DIR}/nginx.conf.tpl" "${CONF_DIR}/nginx.conf"
-perl -pi -e 's/ENV_([_A-Z]+)_ENV/$ENV{$1}/g' "${CONF_DIR}/nginx.conf"
+perl -pi -e 's/ENV_([_A-Z0-9]+)_ENV/$ENV{$1}/g' "${CONF_DIR}/nginx.conf"
 
 ${NGINX_BIN} -g 'daemon off;'
